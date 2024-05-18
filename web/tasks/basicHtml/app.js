20d1cf888c9017ab331b8725e67ac5333e2258bc
zuamaya <a01784238@tec.mx>
2024-05-17 21:55:50 -0600

    Modify rutes for images

--- web/tasks/basicHtml/app.js
+++ web/tasks/basicHtml/app.js
@@ -1,10 +1,15 @@
+/* José Manuel García Zumaya - A01784238*/
+/* 27 - 04 - 2024 */
+/* Basic HTML implementation */
+
 "use strict"
 import express from "express"
 import fs from "fs";
 const app = express();
+app.use(express.static("public"));
 
 app.get('/web', (req, res) => {
-    const file = fs.readFileSync("index.html", "utf-8");
+    const file = fs.readFileSync("public/html/index.html", "utf-8");
     res.status(200).send(file);
 })
 
